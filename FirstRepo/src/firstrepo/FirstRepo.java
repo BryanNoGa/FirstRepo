7a39a8673573b3695d12f09c5c738373b831fe13
BryanNoGa <brayannoga@gmail.com>
2021-09-23 03:02:57 +0200

    Enviando un primer mensaje

---
+++ FirstRepo/src/firstrepo/FirstRepo.java
@@ -0,0 +1,20 @@
+
+package firstrepo;
+
+/**
+ *
+ * @author braya
+ */
+public class FirstRepo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String mundo  = "Hola a todos";
+        
+        System.out.println(mundo);
+    }
+    
+}
