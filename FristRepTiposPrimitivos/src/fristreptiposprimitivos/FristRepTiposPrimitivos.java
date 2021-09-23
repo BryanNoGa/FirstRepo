cdfb5cc72d316c6641f68cf3d30f03f5d1ed266c
BryanNoGa <brayannoga@gmail.com>
2021-09-23 10:34:21 +0200

    Tipos Primitivos

---
+++ FristRepTiposPrimitivos/src/fristreptiposprimitivos/FristRepTiposPrimitivos.java
@@ -0,0 +1,21 @@
+
+package fristreptiposprimitivos;
+
+/**
+ *
+ * @author braya
+ */
+public class FristRepTiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte vartype = 5;
+        
+        System.out.println("Rango minimo del byte"+ Byte.MIN_VALUE);
+        System.out.println("Rango maximo del byte"+ Byte.MAX_VALUE);
+    }
+    
+}
